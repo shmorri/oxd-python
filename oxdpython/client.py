52695b9092b38892e44c5f2d986a3f04e9e9682d
Arunmozhi <arun@arunmozhi.in>
2017-11-24 17:41:25 +0530

    removed duplicate license header

--- oxdpython/client.py
+++ oxdpython/client.py
@@ -1,43 +1,3 @@
-"""
-#
-# Gluu-oxd-library
-#
-# An open source application library for Python
-#
-# This content is released under the MIT License (MIT)
-#
-# Copyright (c) 2017, Gluu inc, USA, Austin
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-#
-# @package	    Gluu-oxd-library
-# @version      3.1.0
-# @author	    Sobhan Panda
-# @author	    sobhan@centroxy.com
-# @copyright	Copyright (c) 2017, Gluu inc federation (https://gluu.org/)
-# @license	    http://opensource.org/licenses/MIT	MIT License
-# @link         https://gluu.org/
-# @since	    Version 3.1.0
-# @filesource
-#/
-"""
-
 import logging
 import urllib2
 import ssl
@@ -183,7 +143,7 @@ class Client:
             return False
 
     def register_site(self, protection_access_token=None):
-        """Function to register the site and generate a unique ID for the site
+        """Function to register the site and generate the oxd_id for the site
 
         Returns:
             string: The ID of the site (also called client id) if the
