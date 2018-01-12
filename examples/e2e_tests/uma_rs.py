f90941f3f50b0b474f0259202340fe8ee23e8d3a
Arunmozhi <arun@arunmozhi.in>
2018-01-13 00:55:27 +0530

    added uma_rs e2e test script

---
+++ examples/e2e_tests/uma_rs.py
@@ -0,0 +1,68 @@
+import os
+import traceback
+import logging
+import argparse
+
+from oxdpython import Client
+
+def run_commands(config):
+    """function that runs the commands for UMA RS app context
+
+    :param config: config file location
+    :return: None
+    """
+    c = Client(config)
+
+    print "\n=> Registering client using register_site()"
+    oxd_id = c.register_site()
+    logging.info("Received: %s", oxd_id)
+
+    print "\n=> Protecting Resource: "
+    resource = {
+        "path": "/photoz",
+        "conditions": [
+            {
+                "httpMethods": ["GET"],
+                "scopes": ["https://photoz.example.com/uma/scope/view"]
+            }
+        ]
+    }
+    print resource
+    protected = c.uma_rs_protect([resource])
+    logging.info("Received: %s", protected)
+
+    print "\n=> Checking Access for URL /photoz, with method GET"
+    status = c.uma_rs_check_access(rpt=None, path='/photoz', http_method='GET')
+    logging.info('Received: %s', status)
+
+if __name__ == '__main__':
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+    config = os.path.join(this_dir, 'uma_rs.cfg')
+    test_config = os.path.join(this_dir, 'test.cfg')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", help="increase output verbosity",
+                        action="store_true")
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.ERROR)
+
+    config = os.path.join(this_dir, 'uma_rs.cfg')
+    test_config = os.path.join(this_dir, 'test.cfg')
+
+
+    with open(test_config, 'w') as of:
+        with open(config) as f:
+            of.write(f.read())
+
+    try:
+        run_commands(test_config)
+    except:
+        print traceback.print_exc()
+
+    os.remove(test_config)
+
+
