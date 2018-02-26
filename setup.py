afa3fca9b296338347382bdacf2bde9cf1b270b0
shmorri <36208047+shmorri@users.noreply.github.com>
2018-02-26 09:02:53 -0600

    Typo fix in line 30
    Just a quick typo fix in the description string in line 30

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     license="MIT",
     author="Gluu",
     author_email="info@gluu.org",
-    description="Python binidings for Gluu oxd server",
+    description="Python bindings for Gluu oxd server",
     long_description="oxd Python is a client library for the Gluu oxd Server. For information, visit http://oxd.gluu.org",
     packages=["oxdpython"],
     zip_safe=False,
